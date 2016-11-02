22f7e74a44d4199bb2820d249d9e09b3c990e908
ob00042 <ob00042@gmail.com>
2016-11-02 18:00:48 +0200

    Book library-without server

---
+++ js/views/book.js
@@ -0,0 +1,21 @@
+var app = app || {};
+
+app.BookView = Backbone.View.extend({
+	tagName: 'div',
+	className: 'bookContainer',
+	template: _.template( $('#bookTemplate').html() ),
+
+	events: {
+		'click .delete': 'deleteBook'
+	},
+
+	render: function() {
+		this.$el.html( this.template( this.model.attributes ) );
+		return this;
+	},
+
+	deleteBook: function() {
+		this.model.destroy();
+		this.remove();
+	}
+});
